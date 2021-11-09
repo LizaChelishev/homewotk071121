2d5931035848f258cb9f6f9164c0640933c2c9a0
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-11-09 13:42:21 +0200

    done

--- question1.py
+++ question1.py
@@ -4,11 +4,17 @@ class Book:
 
 Harry_Potter = Book()
 Harry_Potter.title = 'Harry Potter'
-Harry_Potter.author = '	J. K. Rowling'
+Harry_Potter.author = 'J. K. Rowling'
 Harry_Potter.genre = 'Fantasy'
-Harry_Potter.__dict__['year_of_publish'] = 1997
+Harry_Potter.year_of_publish = 1997
 Harry_Potter.no_of_pages = 572
 
+Winnie_the_Pooh = Book()
+Winnie_the_Pooh.__dict__['title'] = 'Winnie-the-Pooh'
+Winnie_the_Pooh.__dict__['author'] = 'A. A. Milne'
+Winnie_the_Pooh.__dict__['genre'] = 'Children\'s literature'
+Winnie_the_Pooh.__dict__['year_of_publish'] = 1926
+Winnie_the_Pooh.__dict__['no_of_pages'] = 200
 
 def create_book(title, author, genre, year_of_publish, no_of_pages):
     new_book = Book()
@@ -17,3 +23,5 @@ def create_book(title, author, genre, year_of_publish, no_of_pages):
     new_book.year_of_publish = year_of_publish
     new_book.genre = genre
     return new_book
+
+list_of_books =[Harry_Potter, Winnie_the_Pooh, ]
